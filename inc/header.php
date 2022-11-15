999ab7d9f9d29e14c79c78d14d773886e46383e1
maulanahafez <maulanahafez00@gmail.com>
2022-11-15 20:35:34 +0700

    Sinau Gezz v1

---
+++ inc/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
+  <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+  <title>SinauGezz</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+  <div class="page-wrape">
+    <div class="position-relative">
+      <div class="hero-shape"></div>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container py-2"> <a class="navbar-brand font-weight-bold" href="#">SinauGezz</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item"> <a class="nav-link" href="./index.php">Home</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="./bankmateri.php">Bank Materi</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="./banksoal.php">Bank Soal</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="./feedback.php">Feedback</a> </li>
+            </ul>
+            <div class="ml-lg-5"><a class="btn btn-success" href="#">Sign in</a> </div>
+          </div>
+        </div>
+      </nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include('./inc/header.php')
+?>
+<div class="banner-bg position-relative">
+  <div class="container pb-5 pt-md-5">
+    <div class="row py-5 justify-content-between align-items-center">
+      <div class="col-lg-6 col-md-7 py-5">
+        <h1 class="font-weight-bold display-4 mb-4">Our tutors offers face-to-face and online tuition. </h1>
+        <p class="lead mb-5">Our focus - modular training programs from leading practice lectors.</p>
+        <a href="" class="btn btn-success">Sign in  &rarr; </a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<?php
+include('./inc/footer.php')
+?>
